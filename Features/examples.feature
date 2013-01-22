7f4f3970176004f0e4da7fddfeb2fd55f4f08cf3
Ryan Davies <ryan@ryandavies.net>
2013-01-22 00:01:10 +0000

    Added acceptance test for failing tests.

--- Features/examples.feature
+++ Features/examples.feature
@@ -1,43 +1,20 @@
 Feature: Tests
-  As a developer,
-  I want to more easily integrate with OCUnit,
-  In order to run OCUnit tests with a different DSL.
+  In order to run OCUnit tests with a different DSL
+  As a developer
+  I want to more easily integrate with OCUnit
+  
+  Background:
+  Given I have a test suite
+    And I have added Inline as a dependency using CocoaPods
+    And I have successfully installed the pods
   
   Scenario: Passing Tests
-    Given I have a test suite
-      And it has a test case with the content:
-      """
-      #import <SenTestingKit/SenTestingKit.h>
-      #import <Inline/Inline.h>
-      
-      @interface SomeTests : INLTestCase; @end
-      
-      @implementation SomeTests
-      
-      + (void)initialize
-      {
-          [@[@"hello world", @"cat", @"dog"] enumerateObjectsUsingBlock:^(NSString *name, NSUInteger idx, BOOL *stop) {
-              INLTest *test = [[INLTest alloc] init];
-              [test setName:name];
-              [test setBlock:^{}];
-              [self addTestInvocation:[INLTestInvocation invocationWithTest:test]];
-          }];
-      }
-      
-      @end
-      """
-    When I add Inline as a dependency using CocoaPods
-     And I cd to the test suite directory
-     And I successfully run `pod install`
-     And I successfully run the tests
-    Then the output should match:
+    Given the test suite has 3 passing tests
+     When I run the tests
+     Then the output should match:
       """
       Run test suite SomeTests
       Test Suite 'SomeTests' started at .*
-      Run test case hello world
-      Test Case 'hello world' started\.
-      Test Case 'hello world' passed \(.* seconds\)\.
-      
       Run test case cat
       Test Case 'cat' started\.
       Test Case 'cat' passed \(.* seconds\)\.
@@ -45,7 +22,36 @@ Feature: Tests
       Run test case dog
       Test Case 'dog' started\.
       Test Case 'dog' passed \(.* seconds\)\.
+      
+      Run test case rabbit
+      Test Case 'rabbit' started\.
+      Test Case 'rabbit' passed \(.* seconds\)\.
     
       Test Suite 'SomeTests' finished at .*\.
       Executed 3 tests, with 0 failures \(0 unexpected\) in .* seconds
+      """
+      
+  @announce
+  Scenario: Failing Test
+    Given the test suite has 2 passing tests and 1 failing test
+     When I run the tests
+     Then the output should match:
+      """
+      Run test suite SomeTests
+      Test Suite 'SomeTests' started at .*
+      Run test case cat
+      Test Case 'cat' started\.
+      .*/Tests\.m:12: error: cat : Failed!
+      Test Case 'cat' failed \(0\.000 seconds\)\.
+      
+      Run test case dog
+      Test Case 'dog' started\.
+      Test Case 'dog' passed \(0\.000 seconds\)\.
+      
+      Run test case rabbit
+      Test Case 'rabbit' started\.
+      Test Case 'rabbit' passed \(0\.000 seconds\)\.
+      
+      Test Suite 'SomeTests' finished at .*
+      Executed 3 tests, with 1 failure \(0 unexpected\) in .* seconds
       """
--- Features/step_definitions/test_steps.rb
+++ Features/step_definitions/test_steps.rb
@@ -4,14 +4,56 @@ Given /^I have a test suite$/ do
   FileUtils.cp_r(Dir["#{features_path}/Fixtures/Project/*"], "#{@dirs[0]}/Project")
 end
 
-Given /^it has a test case with the content:$/ do |content|
+Given /^the test suite has 3 passing tests$/ do
   f = File.open(File.join(@dirs[0], 'Project', 'Tests', 'Tests.m'), "w")
-  f.write content
+  f.write <<-EOF
+  #import <SenTestingKit/SenTestingKit.h>
+  #import <Inline/Inline.h>
+  @interface SomeTests : INLTestCase; @end
+  @implementation SomeTests
+  + (void)initialize
+  {
+      [@[@"cat", @"dog", @"rabbit"] enumerateObjectsUsingBlock:^(NSString *name, NSUInteger idx, BOOL *stop) {
+          INLTest *test = [[INLTest alloc] init];
+          [test setName:name];
+          [test setBlock:^{}];
+          [self addTestInvocation:[INLTestInvocation invocationWithTest:test]];
+      }];
+  }
+  @end
+  EOF
+  
   f.close
 end
 
-When /^I add Inline as a dependency using CocoaPods$/ do
-  # Add a Podfile including Inline as a dependency:
+Given /^the test suite has 2 passing tests and 1 failing test$/ do
+  f = File.open(File.join(@dirs[0], 'Project', 'Tests', 'Tests.m'), "w")
+  f.write <<-EOF
+  #import <SenTestingKit/SenTestingKit.h>
+  #import <Inline/Inline.h>
+  @interface SomeTests : INLTestCase; @end
+  @implementation SomeTests
+  + (void)initialize
+  {
+      [@[@"cat", @"dog", @"rabbit"] enumerateObjectsUsingBlock:^(NSString *name, NSUInteger idx, BOOL *stop) {
+          INLTest *test = [[INLTest alloc] init];
+          [test setName:name];
+          if (idx == 0) {
+              [test setBlock:^{
+                  [[NSException exceptionWithName:SenTestFailureException reason:@"Failed!" userInfo:@{SenTestFilenameKey: @(__FILE__), SenTestLineNumberKey: @(__LINE__)}] raise];
+              }];
+          } else {
+              [test setBlock:^{}];
+          }
+          [self addTestInvocation:[INLTestInvocation invocationWithTest:test]];
+      }];
+  } 
+  @end
+  EOF
+  f.close
+end
+
+Given /^I have added Inline as a dependency using CocoaPods$/ do
   podfile = File.open(File.join(@dirs[0], 'Project', 'Podfile'), "w")
   podfile.write <<-EOF
 platform :ios
@@ -22,11 +64,23 @@ end
   podfile.close
 end
 
-When /^I cd to the test suite directory$/ do
+Given /^I have successfully installed the pods$/ do
   step "I cd to \"Project\""
+  step "I successfully run `pod install`"
+  step "I cd to \"..\""
 end
 
-When /^I successfully run the tests$/ do
-  step "I successfully run `xcodebuild -project Pods/Pods.xcodeproj -target Pods-Tests -configuration Debug -sdk iphonesimulator6.0 SYMROOT=#{build_path} clean build`"
-  step "I successfully run `xcodebuild -project Project.xcodeproj -target Tests -configuration Debug -sdk iphonesimulator6.0 SYMROOT=#{build_path} clean build`"
+When /^I run the tests$/ do
+  step "I cd to \"Project\""
+  
+  builds = [
+    { "project" => "Pods/Pods.xcodeproj", "target" => "Pods-Tests" },
+    { "project" => "Project.xcodeproj",   "target" => "Tests" }
+  ]
+  
+  builds.each do |build|
+    step "I run `xcodebuild -project #{build["project"]} -target #{build["target"]} -configuration Debug -sdk iphonesimulator6.0 SYMROOT=#{build_path} clean build`"
+  end
+  
+  step "I cd to \"..\""
 end
