0b9182f97dad133197ea70a42a34287104a3f89f
Ryan Davies <ryan@ryandavies.net>
2013-02-11 21:09:50 +0000

    Clean up INLCompilerSpec.

--- Specs/INLCompilerSpec.m
+++ Specs/INLCompilerSpec.m
@@ -9,33 +9,34 @@
 SpecBegin(INLCompiler)
 
 __block INLCompiler *compiler;
-before(^{ compiler = [[INLCompiler alloc] init]; });
+__block NSMutableArray *tests;
 
-describe(@"-invocationsForGroup:", ^{
-    __block NSMutableArray *tests;
-    before(^{
-        tests = [NSMutableArray array];
-        for (NSUInteger i = 0; i < 3; i ++) tests[i] = [[INLTest alloc] init];
-    });
+before(^{
+    compiler = [[INLCompiler alloc] init];
+    tests = [NSMutableArray array];
     
-    it(@"returns invocations for top level tests", ^{
-        INLGroup *group = [[INLGroup alloc] init];
-        for (NSUInteger i = 0; i < 3; i ++) [group addNode:tests[i]];
-        NSArray *invocations = [compiler invocationsForGroup:group];
-        for (NSUInteger i = 0; i < 3; i ++) expect([invocations[i] test]).to.beIdenticalTo(tests[i]);
-    });
+    for (NSUInteger i = 0; i < 3; i ++) {
+        tests[i] = [[INLTest alloc] init];
+    }
+});
     
-    it(@"moves invocations for nested tests to the end of the list", ^{
-        NSMutableArray *groups = [NSMutableArray array];
-        for (NSUInteger i = 0; i < 3; i ++) groups[i] = [[INLGroup alloc] init];
-        [groups[0] addNode:groups[1]];
-        [groups[1] addNode:groups[2]];
-        [groups[0] addNode:tests[0]];
-        [groups[1] addNode:tests[1]];
-        [groups[2] addNode:tests[2]];
-        NSArray *invocations = [compiler invocationsForGroup:groups[0]];
-        for (NSUInteger i = 0; i < 3; i ++) expect([invocations[i] test]).to.beIdenticalTo(tests[i]);
-    });
+it(@"creates invocations for top level tests", ^{
+    INLGroup *group = [[INLGroup alloc] init];
+    for (NSUInteger i = 0; i < 3; i ++) [group addNode:tests[i]];
+    NSArray *invocations = [compiler invocationsForGroup:group];
+    for (NSUInteger i = 0; i < 3; i ++) expect([invocations[i] test]).to.beIdenticalTo(tests[i]);
+});
+
+it(@"moves invocations for nested tests to the end of the list", ^{
+    NSMutableArray *groups = [NSMutableArray array];
+    for (NSUInteger i = 0; i < 3; i ++) groups[i] = [[INLGroup alloc] init];
+    [groups[0] addNode:groups[1]];
+    [groups[1] addNode:groups[2]];
+    [groups[0] addNode:tests[0]];
+    [groups[1] addNode:tests[1]];
+    [groups[2] addNode:tests[2]];
+    NSArray *invocations = [compiler invocationsForGroup:groups[0]];
+    for (NSUInteger i = 0; i < 3; i ++) expect([invocations[i] test]).to.beIdenticalTo(tests[i]);
 });
 
 SpecEnd
