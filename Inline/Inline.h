d4c5acc19b597bdfe67b1513ba46a03e5b727680
Ryan Davies <ryan@ryandavies.net>
2013-03-18 16:09:22 +0000

    Removed INLTestCase and INLInvocation entirely.

--- Inline/Inline.h
+++ Inline/Inline.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2013 Ryan Davies. All rights reserved.
 //
 
-#import "INLTestCase.h"
-#import "INLInvocation.h"
 #import "INLTestRunner.h"
 #import "INLStack.h"
 #import "INLGroup.h"
