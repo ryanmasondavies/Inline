62dcd38dea1b01077aa16982cac3f90ac333a5c4
Ryan Davies <ryan@ryandavies.net>
2013-04-07 12:20:54 +0100

    Added Integrated Tests target for ensuring the format of OCUnit reports.

---
+++ Integrated Tests/INLOCUnitFormatTests.m
@@ -0,0 +1,48 @@
+//
+//  INLOCUnitReporterFormatTests.m
+//  Integrated Tests
+//
+//  Created by Ryan Davies on 07/04/2013.
+//  Copyright (c) 2013 Ryan Davies. All rights reserved.
+//
+
+@interface INLOCUnitReporterFormatTests : SenTestCase
+
+@end
+
+@implementation INLOCUnitReporterFormatTests
+
+- (void)testReporterOutputsPassingTestsInOCUnitFormat
+{
+    // given
+    void(^passingTest)(NSString *) = ^(NSString *label, NSNumber *weight) {
+        id<INLTestState> passed = [[INLPassedState alloc] initWithLabel:label];
+        return [[INLTest alloc] initWithState:passed weight:weight];
+    };
+    void(^groupWithNodes)(NSString *, NSNumber *, NSArray *) = ^(NSString *label, NSNumber *weight, NSArray *nodes) {
+        NSArray *sortDescriptors = @[[[NSSortDescriptor alloc] initWithKey:@"weight" ascending:YES]];
+        CBDSortedArray *sorted = [[CBDSortedArray alloc] initWithObjects:[nodes mutableCopy] sortDescriptors:sortDescriptors];
+        INLGroup *group = [[INLGroup alloc] initWithLabel:@"Group A" nodes:sorted weight:weight];
+    };
+    
+    id<INLNode> subgroup = groupWithNodes(@"Group B", @3, @[passingTest(@"Test C", @1), passingTest(@"Test D", @2)]);
+    id<INLNode> topGroup = groupWithNodes(@"Group A", @1, @[passingTest(@"Test A", @1), passingTest(@"Test B", @2), subgroup]);
+    
+    NSMutableString *output = [[NSMutableString alloc] init];
+    id<INLRunnerDelegate> reporter = [[INLReporter alloc] initWithOutput:output];
+//    reporter = [[INLOCFormatter alloc] initWithOutput:output];
+//    reporter = [[INLOCStatistician alloc] initWithOutput:output];
+//    reporter = [[INLExpander alloc] initWithOutput:output];
+//    reporter = [[INLSeparator alloc] initWithOutput:output];
+    
+    INLRunner *runner = [[INLRunner alloc] initWithDelegate:reporter];
+    
+    // when
+    [runner runByStartingAtNode:topGroup];
+    
+    // then
+    NSString *expected = @"";
+    STAssertEqualObjects(output, expected, @"Output did not match expected value.");
+}
+
+@end
--- Podfile
+++ Podfile
@@ -6,3 +6,7 @@ target 'Unit Tests', exclusive: true do
   pod 'Posit', '~> 0.0.2'
   pod 'OCMock'
 end
+
+target 'Integrated Tests', exclusive: true do
+  pod 'Posit', '~> 0.0.2'
+end
