e34083bf351756cadccea89666f23e5008273956
Ryan Davies <ryan@ryandavies.net>
2013-04-09 12:53:21 +0100

    Added initial publisher protocol.

---
+++ Inline/INLPublisher.h
@@ -0,0 +1,20 @@
+//
+//  INLPublisher.h
+//  Inline
+//
+//  Created by Ryan Davies on 09/04/2013.
+//  Copyright (c) 2013 Ryan Davies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/** Publishes a report to any medium e.g to the system log, to a file, or over the internet. */
+@protocol INLPublisher <NSObject>
+
+/**
+ Publish a report.
+ @param report The report to publish.
+ */
+- (void)publishReport:(NSMutableString *)report;
+
+@end
