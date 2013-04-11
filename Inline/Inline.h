7e70f260db4bd2b249a2e029a9ab5a20f9e56265
Ryan Davies <ryan@ryandavies.net>
2013-04-11 23:03:31 +0100

    Updated main header to match deletions.

--- Inline/Inline.h
+++ Inline/Inline.h
@@ -6,27 +6,9 @@
 //  Copyright (c) 2013 Ryan Davies. All rights reserved.
 //
 
-#import "INLSuite.h"
-
 // composing tests
 #import "INLGroup.h"
 #import "INLHook.h"
 #import "INLTest.h"
-#import "INLReadyState.h"
-#import "INLPendingState.h"
-#import "INLPassedState.h"
-#import "INLFailedState.h"
-#import "INLTimeProvider.h"
-#import "INLStopwatch.h"
 
-// date and time
-#import "INLDateProvider.h"
-#import "INLTimeProvider.h"
-
-// report templates
-#import "INLLlamaCaseStringTransformer.h"
-#import "INLOCUnitTemplateFactory.h"
-
-// running tests
-#import "INLRunner.h"
-#import "INLResultAccumulator.h"
+// compiling tests
