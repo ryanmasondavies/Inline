a9306de97fbb86901bb13a94ccfeabdd2348d541
Ryan Davies <ryan@ryandavies.net>
2013-04-11 21:43:26 +0100

    Removed Features. May be handy again in future, but more effort than they're worth right now.

--- Features/step_definitions/cocoapods_steps.rb
+++
@@ -1,9 +0,0 @@
-Given /^I have added (.*) to my Podfile$/ do |library|
-  write_podfile(File.join(temporary_project_path, 'Podfile'), [library])
-end
-
-Given /^I have successfully installed my pods$/ do
-  step "I cd to \"Project\""
-  step "I successfully run `pod install`"
-  step "I cd to \"..\""
-end
