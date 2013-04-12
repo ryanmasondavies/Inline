d30d14c89a7f9d1bbd4d7a80e7531f56e99be2cf
Ryan Davies <ryan@ryandavies.net>
2013-04-12 15:28:08 +0100

    Import INLSenTestCase in main header.

--- Inline/Inline.h
+++ Inline/Inline.h
@@ -20,6 +20,9 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+// interfacing with SenTestingKit
+#import "INLSenTestCase.h"
+
 // composing tests
 #import "INLGroup.h"
 #import "INLHook.h"
